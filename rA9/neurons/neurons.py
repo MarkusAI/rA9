f9a9cab9f9612d57cad41f11fe4277a63da7fecd
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-09-06 16:19:26 +0900

    Update neurons.py

--- rA9/neurons/neurons.py
+++ rA9/neurons/neurons.py
@@ -1,14 +1,19 @@
+from jax.api import jit
+import jax.numpy as jnp
+
+
 # Integrate And Fire Model
 
 class IF:
     # Vmem => Rest Voltage
 
-    def __init__(self,Vmem,Vth):
+    def __init__(self, Vmem, Vth):
         self.Vmem = Vmem
         self.Vth = Vth
+
     @jit
-    def forward(self,x,v_current):
-        dV = jnp.subtract(x,self.Vmem)
+    def forward(self, x, v_current):
+        dV = jnp.subtract(x, self.Vmem)
         v_current += dV
         spike_list = jnp.greater_equal(v_current, self.Vmem).astype(int)
         v_current = jnp.where(v_current >= self.Vth, self.Vmem, v_current)
@@ -22,17 +27,17 @@ class LIF:
     # tau => Time Constant; Capacity * Resistance
     # Vmem => Rest Voltage
 
-    def __init__(self,tau,Vmem,Vth):
+    def __init__(self, tau, Vmem, Vth):
         self.tau = tau
         self.Vmem = Vmem
         self.Vth = Vth
+
     @jit
-    def forward(self,x,v_current):
-        dV_tau = jnp.subtract(x,self.Vmem)
-        dV = jnp.divide(dV_tau,self.tau)
+    def forward(self, x, v_current):
+        dV_tau = jnp.subtract(x, self.Vmem)
+        dV = jnp.divide(dV_tau, self.tau)
         v_current += dV
         spike_list = jnp.greater_equal(v_current, self.Vmem).astype(int)
         v_current = jnp.where(v_current >= self.Vth, self.Vmem, v_current)
 
         return spike_list, v_current
-
