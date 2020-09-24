fca03af6ee7beac0d5dbbd28829695520114a73e
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-09-24 20:56:14 +0900

    Update LIF.py

--- rA9/neurons/LIF.py
+++ rA9/neurons/LIF.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+from jax.ops import index, index_add
 
 
 class LIF(object):
@@ -11,22 +12,20 @@ class LIF(object):
         self.Vth = Vth
         self.dt = dt
 
-    @staticmethod
     def forward(self, x, v_current):
         dV_tau = jnp.divide(jnp.subtract(x, self.Vmem), self.dt)
         dV = jnp.divide(dV_tau, self.tau)
-        v_current += dV
+        v_current = index_add(v_current, index[:], dV)
         spike_list = jnp.greater_equal(v_current, self.Vmem).astype(int)
-        v_current = jnp.where(v_current >= self.Vth, self.Vmem, v_current*jnp.exp(-1/self.tau_m))
+        v_current = jnp.where(v_current >= self.Vth, self.Vmem, v_current * jnp.exp(-1 / self.tau_m))
 
         return spike_list, v_current
-        
-    @staticmethod
+
     def backward(self, time, spike_list, weights, e_gradient):
         gamma = spike_list[0]
-        t_Tk_divby_tau_m = jnp.divide(jnp.subtract(time,spike_list[1]), self.tau_m)
-        f_prime_t = jnp.multiply(jnp.exp(t_Tk_divby_tau_m), (-1/self.tau_m))
-        aLIFnet = jnp.multiply(1/self.Vth,(1+jnp.multiply(jnp.divide(1, gamma),f_prime_t)))
+        t_Tk_divby_tau_m = jnp.divide(jnp.subtract(time, spike_list[1]), self.tau_m)
+        f_prime_t = jnp.multiply(jnp.exp(t_Tk_divby_tau_m), (-1 / self.tau_m))
+        aLIFnet = jnp.multiply(1 / self.Vth, (1 + jnp.multiply(jnp.divide(1, gamma), f_prime_t)))
         d_w = jnp.matmul(weights, e_gradient)
 
         return jnp.multiply(d_w, aLIFnet)
