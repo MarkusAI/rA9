cf7035f941569774d2a80c40474d95cec5a88c09
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-09-24 22:02:33 +0900

    Update Output.py

--- rA9/neurons/Output.py
+++ rA9/neurons/Output.py
@@ -3,13 +3,12 @@ import jax.numpy as jnp
 
 
 class Output(object):
-    def __init__(self, tau_m, Vmem, dt):
+    def __init__(self, tau_m, dt):
         self.tau_m = tau_m
-        self.Vmem = Vmem
         self.dt = dt
 
     def forward(self, x, v_current, time_step):
-        dV_tau = jnp.divide(jnp.subtract(x, self.Vmem), self.dt)
+        dV_tau = jnp.multiply(jnp.subtract(x, v_current), self.dt)
         dV = jnp.divide(dV_tau, self.tau_m)
         v_current = index_add(v_current, index[:], dV)
-        return jnp.divide(jnp.where(v_current >= self.Vth, self.Vmem, v_current * jnp.exp(-1 / self.tau_m)), time_step*self.dt)
+        return jnp.divide(v_current * jnp.exp(-1 / self.tau_m), time_step*self.dt)
