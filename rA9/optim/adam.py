aeb2c5ca10114bd028b0e41ae699cf8de87bea36
jepetolee <55366212+jepetolee@users.noreply.github.com>
2020-10-08 18:21:50 +0900

    i've found that d_weights doesn't connected with those ones
    dd

--- rA9/optim/adam.py
+++ rA9/optim/adam.py
@@ -7,6 +7,7 @@ class Adam(Optimizer):
     def __init__(self, params,d_weights, lr=0.001, betas=(0.88, 0.999), eps=2e-7, weight_decay=0):
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
         super(Adam, self).__init__(params, defaults)
+        self.d_weights =d_weights
 
     def step(self, closure=None):
         loss = None
@@ -21,7 +22,7 @@ class Adam(Optimizer):
                     state['step'] = 0
                     state['exp_avg'] = jnp.zeros(p.shape)
                     state['exp_avg_sq'] = jnp.zeros(p.shape)
-                d_w= d_weights[count]
+                d_w= self.d_weights[count]
                 exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
                 beta1, beta2 = group['betas']
 
