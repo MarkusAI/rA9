1d7e50cd5d5649ed9b07b4eb7cfc1363ffcd5b4b
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-10-19 22:57:20 +0900

    Delete spike.py

--- rA9/nn/spike.py
+++
@@ -1,9 +0,0 @@
-from rA9.autograd import Variable
-
-
-class Spike(Variable):
-    def __init__(self, data=None):
-        return super(Spike, self).__init__(data, requires_grad=True)
-
-    def __repr__(self):
-        return 'Spike containing:' + self.data.__repr__()
