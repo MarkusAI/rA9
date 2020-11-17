2df6713e934f42bc648d37ebea40e2affea66d6c
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-17 16:35:29 +0900

    Update activation.py

--- rA9/nn/_functions/activation.py
+++ rA9/nn/_functions/activation.py
@@ -11,28 +11,27 @@ class LIF(Function):
     def forward(ctx, input, v_current, tau_m, Vth, dt, s_time_list, time, gamma):
         assert isinstance(input, Variable)
 
-        def np_fn(input_np, v_current,gamma, tau_m, Vth, dt):
-            v_current = ((input_np-v_current)/tau_m)*dt
+        def np_fn(input_np, v_current, gamma, tau_m, Vth, dt):
+            v_current = ((input_np - v_current) / tau_m) * dt
             spike = np.greater_equal(v_current + np.multiply(np.divide(np.subtract(input_np, v_current), tau_m), dt),
                                      Vth).astype('float32')
 
-            gamma += np.where(spike>=Vth, 1, 0)
+            gamma += np.where(spike >= Vth, 1, 0)
             return spike, v_current, gamma
-
 
         def grad_fn(grad_outputs, s_time_list, time, tau_m, gamma, Vth):
             return np.multiply(grad_outputs, (1 / Vth * (
-                        1 + np.multiply(1 / gamma, np.sum(np.multiply(-1 / tau_m, np.exp(time - s_time_list)))))))
+                    1 + np.multiply(1 / gamma, np.sum(np.multiply(-1 / tau_m, np.exp(time - s_time_list)))))))
 
         np_args = (input.data, v_current.data, gamma.data, tau_m, Vth, dt)
-        spike, v_current,gamma = jit(np_fn)(*np_args)
+        spike, v_current, gamma = jit(np_fn)(*np_args)
 
         spike_time = spike * time
         s_time_list = np.concatenate((spike_time, s_time_list.data))
         grad_np_args = (s_time_list, time, tau_m, gamma, Vth)
         id = "LIF"
 
-        return grad_fn, grad_np_args, spike, v_current,gamma, id
+        return grad_fn, grad_np_args, spike, v_current, gamma, s_time_list, id
 
     @staticmethod
     def backward(ctx, grad_output):
