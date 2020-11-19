914ae4986788ef684f5ee20f76b2e68209d407e4
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-19 21:26:28 +0900

    Update output.py

--- rA9/nn/modules/output.py
+++ rA9/nn/modules/output.py
@@ -9,13 +9,11 @@ class Output(Module):
     def __init__(self, out_features, tau_m=0.1, dt=1, Vth=1):
         super(Output, self).__init__()
         self.out_features = out_features
-        self.weight = Parameter(jnp.zeros(shape=(out_features, out_features)))
-        self.gamma = Variable(jnp.zeros(shape=(1, out_features)))
         self.v_current = self.v_currentT(None)
         self.tau_m = tau_m
         self.time_step = 1
-        self.dt = dt
         self.Vth = Vth
+        self.dt = dt
 
         self.reset_parameters()
 
@@ -47,6 +45,4 @@ class Output(Module):
         return out, time + self.dt * self.time_step
 
     def reset_parameters(self):
-        size = self.weight.data.shape
-        stdv = jnp.divide(1.0, jnp.sqrt(size[1]))
-        self.weight.uniform(-stdv, stdv)
+        pass
