ea7657f638c070e67a43a52d27362bcab1b5d680
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-19 22:00:27 +0900

    Update output.py

--- rA9/nn/_functions/output.py
+++ rA9/nn/_functions/output.py
@@ -8,19 +8,18 @@ class Output(Function):
     id = "output"
 
     @staticmethod
-    def forward(ctx, input, v_current, tau_m, dt, time_step, Vth, gamma):
-        assert isinstance(input, Variable)
+    def forward(ctx, input, v_current, tau_m, dt, time_step):
         assert isinstance(v_current, Variable)
-        assert isinstance(weights, Variable)
+        assert isinstance(input, Variable)
 
-        def np_fn(input_np, weights_np, v_current, time_step, dt, tau_m):
+        def np_fn(input_np, v_current, time_step, dt, tau_m):
             return jnp.divide(
                 jnp.multiply(jnp.subtract(input_np, v_current), dt * tau_m)+v_current, time_step)
 
         def grad_fn(grad_outputs, input, s_time_list, time, tau_m, gamma, Vth):
             return (jnp.divide(grad_outputs,time), jnp.matmul(input.T,jnp.divide(grad_outputs,time)))
 
-        np_args = (input.data, weights.data, v_current.data, time_step, dt, tau_m)
+        np_args = (input.data, v_current.data, time_step, dt, tau_m)
         spike = jit(np_fn)(*np_args)
 
         grad_np_args = (spike, input.data, time_step, tau_m, gamma.data, Vth)
