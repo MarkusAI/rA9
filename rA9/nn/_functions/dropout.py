5e4129d35476a58f9774c2d5db20048bf889115f
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-23 21:34:35 +0900

    Update dropout.py

--- rA9/nn/_functions/dropout.py
+++ rA9/nn/_functions/dropout.py
@@ -1,7 +1,7 @@
+from jax import lax
+import jax.numpy as jnp
 from rA9.autograd import Function
 from rA9.autograd import Variable
-import jax.numpy as jnp
-
 
 class Dropout(Function):
     id = "Dropout"
@@ -12,7 +12,7 @@ class Dropout(Function):
         def np_fn(input_np, noise):
             return input_np * noise
 
-        noise = jnp.random.binomial(1, p, size=input.data.shape)
+        noise = binomial(1, p, shape=input.data.shape)
         if not train:
             noise.fill(1)
         if p == 1:
@@ -24,3 +24,15 @@ class Dropout(Function):
     @staticmethod
     def backward(ctx, grad_output):
         return super(Dropout, Dropout).backward(ctx, grad_output)
+
+    
+def binomial(key, p, n=1, shape=()):
+    p, n = _promote_shapes(p, n)
+    shape = shape or lax.broadcast_shapes(np.shape(p), np.shape(n))
+    n_max = np.max(n)
+    uniforms = random.uniform(key, shape + (n_max,))
+    n = np.expand_dims(n, axis=-1)
+    p = np.expand_dims(p, axis=-1)
+    mask = (np.arange(n_max) > n).astype(uniforms.dtype)
+    p, uniforms = promote_shapes(p, uniforms)
+    return np.sum(mask * lax.lt(uniforms, p), axis=-1, keepdims=False)
