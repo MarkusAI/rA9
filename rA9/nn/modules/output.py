533ec7514349e74390a70c414e41631a5a36e246
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-27 09:37:36 +0900

    Update output.py

--- rA9/nn/modules/output.py
+++ rA9/nn/modules/output.py
@@ -1,15 +1,15 @@
 import jax.numpy as jnp
 from .module import Module
 from .. import functional as F
-from rA9.nn.parameter import Parameter
-from rA9.autograd.variable import *
+from rA9.autograd.variable import Variable
 
 
 class Output(Module):
+    v_current = None
+
     def __init__(self, out_features, tau_m=0.1, dt=1, Vth=1):
         super(Output, self).__init__()
         self.out_features = out_features
-        self.v_current = self.v_currentT(None)
         self.tau_m = tau_m
         self.time_step = 1
         self.Vth = Vth
@@ -17,31 +17,21 @@ class Output(Module):
 
         self.reset_parameters()
 
-    class v_currentT(object):
-        def __init__(self, v_current):
-            self.v_current = v_current
-
-        def init_v_current(self, size):
-            self.v_current = Variable(jnp.zeros(shape=size))
-            return self.v_current
-
-        def save_v_current(self, v_current):
-            self.v_current = v_current
-            return self.v_current
-
-        def recall_v_current(self):
-            return self.v_current
-
     def forward(self, input, time, activetime):
         if activetime == 0:
-            v_current = self.v_current.init_v_current(size=(1, self.out_features))
+            Output.v_current = Variable(jnp.zeros(shape=(1, self.out_features)))
+            out, v_current_ret = F.Output(input=input,
+                                          v_current=Output.v_current,
+                                          tau_m=self.tau_m, dt=self.dt,
+                                          time_step=time + self.time_step)
+            Output.v_current = v_current_ret
         else:
-            v_current = self.v_current.recall_v_current()
-        out, v_current_ret = F.Output(input=input,
-                                      v_current=v_current,
-                                      tau_m=self.tau_m, dt=self.dt,
-                                      time_step=time + self.time_step)
-        self.v_current.save_v_current(v_current_ret)
+            out, v_current_ret = F.Output(input=input,
+                                          v_current=Output.v_current,
+                                          tau_m=self.tau_m, dt=self.dt,
+                                          time_step=time + self.time_step)
+            Output.v_current = v_current_ret
+
         return out, time + self.dt * self.time_step
 
     def reset_parameters(self):
