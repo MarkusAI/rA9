950aaefb1c559981227f1bd733338b6750fd08dd
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-27 09:12:35 +0900

    Update activation.py

--- rA9/nn/modules/activation.py
+++ rA9/nn/modules/activation.py
@@ -1,58 +1,47 @@
+import jax.numpy as jnp
 from .module import Module
 from .. import functional as F
-from rA9.nn.parameter import Parameter
 from rA9.autograd.variable import Variable
-import jax.numpy as jnp
 
 
 class LIF(Module):
+    gamma = None
+    spike_time_list = None
+    v_current = None
+
     def __init__(self, tau_m=100, Vth=1, dt=1):
         super(LIF, self).__init__()
         self.time_step = 1
         self.tau_m = tau_m
         self.Vth = Vth
         self.dt = dt
-        self.gamma = self.including_object(None)
-        self.spike_time_list = self.including_object(None)
-        self.v_current = self.including_object(None)
-
-    class including_object(object):
-        def __init__(self, data):
-            self.data = data
-
-        def init_data(self, size):
-            self.data = Variable(jnp.zeros(shape=size))
-            return self.data
-
-        def save_data(self, new_data):
-            self.data = new_data
-            return self.data
-
-        def recall_data(self):
-            return self.data
 
     def forward(self, input, time, activetime):
-
         if activetime == 0:
-            v_current = self.v_current.init_data(input.data.shape)
-            gamma = self.gamma.init_data(input.data.shape)
-            spike_time_list = self.spike_time_list.init_data(input.data.shape)
+            LIF.v_current = Variable(jnp.zeros(shape=input.data.shape))
+            LIF.gamma = Variable(jnp.zeros(input.data.shape))
+            LIF.spike_time_list = Variable(jnp.zeros(input.data.shape))
+
+            out, v_current, gamma, spike_time_list = F.LIF(input, LIF.v_current, self.tau_m, self.Vth, self.dt,
+                                                           LIF.spike_time_list, time,
+                                                           LIF.gamma)
+            LIF.spike_time_list = spike_time_list
+            LIF.v_current = v_current
+            LIF.gamma = gamma
+            
         else:
-            v_current = self.v_current.recall_data()
-            gamma = self.gamma.recall_data()
-            spike_time_list = self.spike_time_list.recall_data()
+            spike_time_list = LIF.spike_time_list
+            v_current = LIF.v_current
+            gamma = LIF.gamma
 
-        out, v_current_ret, gamma_t ,spike_time_list_ret = F.LIF(input, v_current, self.tau_m, self.Vth, self.dt, spike_time_list, time,
-                                               gamma)
+            out, v_current, gamma, spike_time_list = F.LIF(input, v_current, self.tau_m, self.Vth, self.dt,
+                                                           spike_time_list, time,
+                                                           gamma)
+            LIF.spike_time_list = spike_time_list
+            LIF.v_current = v_current
+            LIF.gamma = gamma
 
-
-        self.gamma.save_data(gamma_t)
-
-        self.v_current.save_data(v_current_ret)
-
-        self.spike_time_list.save_data(spike_time_list_ret)
-
-        return out , time+self.dt*self.time_step
+        return out, time + self.dt * self.time_step
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' \
