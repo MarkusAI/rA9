5cbc9a5ee71262bad2ae033c1f5ffdb04eeaf017
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-28 02:16:07 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rA9",
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     author="Dongyeong Kim, Jaeseok Lee, Junho Yeo",
     author_email="dongyeongkim33@gmail.com, jepetolee@gmail.com, hanaro0704@gmail.com",
