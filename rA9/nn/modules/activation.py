8d6790b9927176b13d0ab6a056a37e3b2d98bcd4
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-28 00:04:36 +0900

    Update activation.py

--- rA9/nn/modules/activation.py
+++ rA9/nn/modules/activation.py
@@ -5,41 +5,42 @@ from rA9.autograd.variable import Variable
 
 
 class LIF(Module):
-    gamma = None
-    spike_time_list = None
-    v_current = None
+    
 
     def __init__(self, tau_m=100, Vth=1, dt=1):
         super(LIF, self).__init__()
-        self.time_step = 1
+        self.spike_time_list = None
+        self.v_current = None
+        self.gamma = None
         self.tau_m = tau_m
+        self.time_step = 1
         self.Vth = Vth
         self.dt = dt
 
     def forward(self, input, time, activetime):
         if activetime == 0:
-            LIF.v_current = Variable(jnp.zeros(shape=input.data.shape))
-            LIF.gamma = Variable(jnp.zeros(input.data.shape))
-            LIF.spike_time_list = Variable(jnp.zeros(input.data.shape))
+            self.v_current = Variable(jnp.zeros(shape=input.data.shape))
+            self.gamma = Variable(jnp.zeros(input.data.shape))
+            self.spike_time_list = Variable(jnp.zeros(input.data.shape))
 
             out, v_current, gamma, spike_time_list = F.LIF(input, LIF.v_current, self.tau_m, self.Vth, self.dt,
                                                            LIF.spike_time_list, time,
                                                            LIF.gamma)
-            LIF.spike_time_list = spike_time_list
-            LIF.v_current = v_current
-            LIF.gamma = gamma
+            self.spike_time_list = spike_time_list
+            self.v_current = v_current
+            self.gamma = gamma
             
         else:
-            spike_time_list = LIF.spike_time_list
-            v_current = LIF.v_current
-            gamma = LIF.gamma
+            spike_time_list = self.spike_time_list
+            v_current = self.v_current
+            gamma = self.gamma
 
             out, v_current, gamma, spike_time_list = F.LIF(input, v_current, self.tau_m, self.Vth, self.dt,
                                                            spike_time_list, time,
                                                            gamma)
-            LIF.spike_time_list = spike_time_list
-            LIF.v_current = v_current
-            LIF.gamma = gamma
+            self.spike_time_list = spike_time_list
+            self.v_current = v_current
+            self.gamma = gamma
 
         return out, time + self.dt * self.time_step
 
