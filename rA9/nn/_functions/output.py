459c588b2b82df26cc5f500c94d6f7e054562b71
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-28 01:47:52 +0900

    Update output.py

--- rA9/nn/_functions/output.py
+++ rA9/nn/_functions/output.py
@@ -1,33 +1,32 @@
+from jax import jit
 import jax.numpy as jnp
-from .module import Module
-from .. import functional as F
-from rA9.autograd.variable import Variable
+from rA9.autograd import Function
+from rA9.autograd import Variable
 
 
-class Output(Module):
+class Output(Function):
+    id = "output"
 
-    def __init__(self, out_features, tau_m=0.1, dt=1, Vth=1):
-        super(Output, self).__init__()
-        self.out_features = out_features
-        self.v_current = None
-        self.tau_m = tau_m
-        self.time_step = 1
-        self.Vth = Vth
-        self.dt = dt
+    @staticmethod
+    def forward(ctx, input, v_current, tau_m, dt, time_step):
+        assert isinstance(v_current, Variable)
+        assert isinstance(input, Variable)
 
-        self.reset_parameters()
+        def np_fn(input_np, v_current, time_step, dt, tau_m):
+            return jnp.divide(
+                jnp.multiply(jnp.subtract(input_np, v_current), dt * tau_m)+v_current, time_step)
 
-    def forward(self, input, time, activetime):
-        if activetime == 0:
-            self.v_current = Variable(jnp.zeros(shape=(1, self.out_features)))
+        def grad_fn(grad_outputs, input, s_time_list, time, tau_m, gamma, Vth):
+            return (jnp.divide(grad_outputs,time), jnp.matmul(input.T,jnp.divide(grad_outputs,time)))
 
-        out, v_current_ret = F.Output(input=input,
-                                      v_current=self.v_current,
-                                      tau_m=self.tau_m, dt=self.dt,
-                                      time_step=time + self.time_step)
-        self.v_current = v_current_ret
+        np_args = (input.data, v_current.data, time_step, dt, tau_m)
+        spike = jit(np_fn)(*np_args)
 
-        return out, time + self.dt * self.time_step
+        grad_np_args = (spike, input.data, time_step, tau_m, 0, 0)
 
-    def reset_parameters(self):
-        pass
+        id = "output"
+        return grad_fn, grad_np_args, spike, v_current.data, id
+
+    @staticmethod
+    def backward(ctx, grad_outputs):
+        super(Output, Output).backward(ctx, grad_outputs)
