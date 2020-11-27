0626662c4a3132c07c6890ba972e1627f0946737
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-28 00:11:49 +0900

    Update output.py

--- rA9/nn/modules/output.py
+++ rA9/nn/modules/output.py
@@ -5,11 +5,11 @@ from rA9.autograd.variable import Variable
 
 
 class Output(Module):
-    v_current = None
 
     def __init__(self, out_features, tau_m=0.1, dt=1, Vth=1):
         super(Output, self).__init__()
         self.out_features = out_features
+        self.v_current = None
         self.tau_m = tau_m
         self.time_step = 1
         self.Vth = Vth
@@ -19,18 +19,18 @@ class Output(Module):
 
     def forward(self, input, time, activetime):
         if activetime == 0:
-            Output.v_current = Variable(jnp.zeros(shape=(1, self.out_features)))
+            self.v_current = Variable(jnp.zeros(shape=(1, self.out_features)))
             out, v_current_ret = F.Output(input=input,
                                           v_current=Output.v_current,
                                           tau_m=self.tau_m, dt=self.dt,
                                           time_step=time + self.time_step)
-            Output.v_current = v_current_ret
+            self.v_current = v_current_ret
         else:
             out, v_current_ret = F.Output(input=input,
                                           v_current=Output.v_current,
                                           tau_m=self.tau_m, dt=self.dt,
                                           time_step=time + self.time_step)
-            Output.v_current = v_current_ret
+            self.v_current = v_current_ret
 
         return out, time + self.dt * self.time_step
 
