a8d29fb300aff7efc57fc4beaa55d819dc50da65
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-11-28 18:58:20 +0900

    Update cnn_mnist.py

--- example/cnn_mnist.py
+++ example/cnn_mnist.py
@@ -65,8 +65,8 @@ for epoch in range(15):
 
         for t, q in enumerate(pe.Encoding(data)):
             data = Variable(q, requires_grad=True)
-
-        output, time = model(data, t)
+            output, time = model(data, t)
+            
         loss = F.Spikeloss(output, target, time_step=time)
         loss.backward()  # calc gradients
         optimizer.step()  # update gradients
