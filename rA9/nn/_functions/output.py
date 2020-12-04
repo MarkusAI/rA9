a55fd7c0200f0eed563bbb23bd2b0038af8cdee6
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-12-05 01:56:24 +0900

    Update output.py

--- rA9/nn/_functions/output.py
+++ rA9/nn/_functions/output.py
@@ -13,19 +13,19 @@ class Output(Function):
         assert isinstance(input, Variable)
 
         def np_fn(input_np, v_current, time_step, dt, tau_m):
-            return input_np+v_current*jnp.exp(-1/tau_m)
+            return (input_np+v_current*jnp.exp(-1/tau_m))/time_step, input_np+v_current*jnp.exp(-1/tau_m)
 
-        def grad_fn(grad_outputs, input, s_time_list, time, tau_m, gamma, Vth):
-            return (jnp.divide(grad_outputs,time), jnp.matmul(input.T,jnp.divide(grad_outputs,time)))
+        def grad_fn(grad_outputs, input, s_time_list, time, tau_m, gamma):
+            return (jnp.divide(grad_outputs, time), jnp.matmul(input.T, jnp.divide(grad_outputs, time)))
         
         
         np_args = (input.data, v_current.data, time_step, dt, tau_m)
-        v_current = jit(np_fn)(*np_args)
+        out, v_current = jit(np_fn)(*np_args)
 
         grad_np_args = (input.data, time_step, tau_m, 0, 0)
 
         id = "output"
-        return grad_fn, grad_np_args, v_current, id
+        return grad_fn, grad_np_args, out, v_current, id
 
     @staticmethod
     def backward(ctx, grad_outputs):
