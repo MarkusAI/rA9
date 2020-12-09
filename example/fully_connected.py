e766091f5e6fecfef963babeb7126a26f56ca68a
jepetolee <jepetolee@gmail.com>
2020-12-09 14:34:48 +0900

    fix

--- example/fully_connected.py
+++ example/fully_connected.py
@@ -1,13 +1,13 @@
 import rA9.nn as nn
-from rA9.utils import tpu
-from rA9.optim import Adam
+from rA9.optim import SGD
+import jax.random as random
 import rA9.nn.functional as F
 from rA9.autograd import Variable
 from rA9.nn.modules import Module
 from rA9.utils import PoissonEncoder
 from rA9.utils.data import DataLoader
 from rA9.datasets.mnist import MnistDataset, collate_fn
-
+import  jax.numpy as jnp
 
 
 class SNN(Module):
@@ -21,8 +21,8 @@ class SNN(Module):
         
 
     def forward(self, x, time):
-      x, intime = self.active1(self.fc1(x), time, time)
-      return self.output(self.fc2(x), intime, time)
+      x, intime = self.active1(self.fc1(x), time)
+      return self.output(self.fc2(x), time)
 
 
 model = SNN()
@@ -31,7 +31,7 @@ model.train()
 PeDurx = 70
 batch_size = 32
 Pencoder = PoissonEncoder(duration=PeDurx)
-optimizer = Adam(model.parameters(), lr=0.01)
+optimizer = SGD(model.parameters(), lr=0.01)
 
 train_loader = DataLoader(dataset=MnistDataset(training=True, flatten=True),
                           collate_fn=collate_fn, shuffle=True,
@@ -44,11 +44,12 @@ test_loader = DataLoader(dataset=MnistDataset(training=False, flatten=True),
 for epoch in range(15):
     for i, (data, target) in enumerate(train_loader):
         target = Variable(target)
-        for t, q in enumerate(Pencoder.Encoding(data)):
-
-            data = Variable(q, requires_grad=True)
-
-            output, time = model(data, t)
+        for t in range(PeDurx):
+            rnum = random.uniform(key=random.PRNGKey(0), shape=data.shape)
+            uin = (jnp.abs(data) / 2 > rnum).astype('float32')
+            q = jnp.multiply(uin, jnp.sign(data))
+            output, time = model(Variable(q), t)
+            print(str(t))
 
         loss = F.Spikeloss(output, target, time_step=time)
         loss.backward()  # calc gradients
