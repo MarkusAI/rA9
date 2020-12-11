08885a17faad7a140f3806e39996327d31c83fc8
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-12-11 11:21:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.2.9',
     license='GPLv3',
     author="Dongyeong Kim, Jaeseok Lee, Junho Yeo, Yoongi Kim",
-    author_email="dongyeongkim33@gmail.com, jepetolee@gmail.com, hanaro0704@gmail.com",
+    author_email="dongyeongkim33@gmail.com, jepetolee@gmail.com, hanaro0704@gmail.com, naver7731@naver.com",
     description="Spiking Neural Network Library based on the spike-based error backpropagation",
     long_description=open('README.md').read(),
     url='https://github.com/MarkusAI/rA9',
