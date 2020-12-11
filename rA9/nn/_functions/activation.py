ac2887bbae1698140732847ea67c74e84381f60b
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-12-11 15:27:14 +0900

    Update activation.py

--- rA9/nn/_functions/activation.py
+++ rA9/nn/_functions/activation.py
@@ -18,15 +18,15 @@ class LIF(Function):
             return spike, jnp.multiply(jnp.exp(-1 / tau_m), v_current), gamma + spike.astype('int32')
 
         def grad_fn(grad_outputs, s_time_list, time, tau_m, gamma, Vth):
-            return jnp.multiply(grad_outputs, (1 / Vth * (
-                    1 + jnp.multiply(1 / gamma, jnp.sum(jnp.multiply(-1 / tau_m, jnp.exp((-1 / tau_m)*(time - s_time_list))))))))
+            return jnp.multiply(grad_outputs, 1 / Vth * (
+                    1 + jnp.multiply(1 / gamma, jnp.sum(jnp.multiply(-1 / tau_m, jnp.exp((-1 / tau_m)*(time - s_time_list)))))))
 
         np_args = (input.data, v_current.data, gamma.data, tau_m, Vth, dt)
         spike, v_current, gamma = jit(np_fn)(*np_args)
 
         spike_time = spike * time
         s_time_list = jnp.concatenate((spike_time, s_time_list.data))
-        grad_np_args = (spike, time, tau_m, gamma, Vth)
+        grad_np_args = (s_time_list, time, tau_m, gamma, Vth)
 
         id = "LIF"
 
