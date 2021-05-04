45e007999e69a058e8bb059464b90203f8855968
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2021-05-05 00:02:47 +0900

    Update mnist.py

--- rA9/datasets/mnist.py
+++ rA9/datasets/mnist.py
@@ -25,7 +25,7 @@ def download(url, filename):
 
 
 def mnist():
-    base_url = 'http://yann.lecun.com/exdb/mnist/'
+    base_url = 'https://ossci-datasets.s3.amazonaws.com/mnist'
 
     def parse_labels(filename):
         with gzip.open(filename, 'rb') as fh:
