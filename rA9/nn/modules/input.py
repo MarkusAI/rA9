74b731498f32d3092e6fae69cc9140a631f3543a
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2021-08-12 01:57:22 +0900

    Update input.py

--- rA9/nn/modules/input.py
+++ rA9/nn/modules/input.py
@@ -1,32 +1,16 @@
-import jax.numpy as jnp
 from .module import Module
 from .. import functional as F
-from rA9.autograd.variable import Variable
 
 
 class Input(Module):
 
-    def __init__(self, tau_m=100, Vth=1, dt=1):
+    def __init__(self):
         super(Input, self).__init__()
-        self.tau_m = tau_m
-        self.time_step = 1
-        self.Vth = Vth
-        self.dt = dt
+
+    def forward(self, input):
+        return F.Input(input)
+
         
-
-    def forward(self, input, time):
-        if time == 0:
-            self.v_current = Variable(jnp.zeros(shape=input.data.shape))
-            self.gamma = Variable(jnp.zeros(shape=input.data.shape))
-            self.spike_time_list = Variable(jnp.zeros(shape=input.data.shape))
-        out, v_current, gamma, spike_time_list = F.Input(input, self.v_current, self.tau_m, self.Vth, self.dt,
-                                                       self.spike_time_list, time,
-                                                       self.gamma)
-        self.spike_time_list = spike_time_list
-        self.v_current = v_current
-        self.gamma = gamma
-
-        return out
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' \
