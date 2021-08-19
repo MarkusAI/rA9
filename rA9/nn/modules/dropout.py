1d18e7de7ae1b53dddda861d83eaf6605927b3a5
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2021-08-19 15:58:49 +0900

    Update dropout.py

--- rA9/nn/modules/dropout.py
+++ rA9/nn/modules/dropout.py
@@ -1,23 +1,22 @@
-import numpy as np
 from .module import Module
 from .. import functional as F
 
 
 class Dropout(Module):
 
-    def __init__(self, p=0.2):
+    def __init__(self, p=0.2, training=True):
         super(Dropout, self).__init__()
         if p < 0 or p > 1:
             raise ValueError("dropout probability has to be between 0 and 1, "
                              "but got {}".format(p))
         self.p = p
-        self.mask = None
+        self.training = training
     def forward(self, input):
-        self.mask = np.random.rand(*input.shape) > self.p
-        return F.dropout(input, self.mask, self.p, self.training)
+        return F.dropout(input, self.p, self.training)
 
     def __repr__(self):
         inplace_str = ', inplace' if self.inplace else ''
         return self.__class__.__name__ + ' (' \
             + 'p = ' + str(self.p) \
             + inplace_str + ')'
+    
